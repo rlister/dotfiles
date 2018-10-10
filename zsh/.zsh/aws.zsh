37ed217f51673f5f72e33334feb66f187a5c5e7e
Richard Lister <rlister@gmail.com>
2018-10-10 12:16:51 -0400

    aws-cli completion

--- zsh/.zsh/aws.zsh
+++ zsh/.zsh/aws.zsh
@@ -1,3 +1,6 @@
+## aws-cli cmdline completion
+source /usr/local/bin/aws_zsh_completer.sh
+
 ## switching creds with https://github.com/99designs/aws-vault
 alias ce='aws-vault exec'
 function av() {aws-vault exec $*}
