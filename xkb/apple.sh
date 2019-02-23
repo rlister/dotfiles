62748462e7b7bf4e8cde8c23f14f2a956d5a4707
Richard Lister <rlister@gmail.com>
2019-02-23 12:33:53 -0500

    fix country code

--- xkb/apple.sh
+++ xkb/apple.sh
@@ -1,5 +1,5 @@
 ## clear all options
-setxkbmap -model "pc105" -layout "hr,us" -option ""
+setxkbmap -model "pc101" -layout "us" -option ""
 
 ## set the Apple keyboard
-setxkbmap -rules "evdev" -model "pc105" -layout "hr,us" -option "terminate:ctrl_alt_bksp,lv3:rwin_switch,grp:shifts_toggle,caps:ctrl_modifier,altwin:swap_lalt_lwin"
+setxkbmap -rules "evdev" -model "pc101" -layout "us" -option "terminate:ctrl_alt_bksp,lv3:rwin_switch,grp:shifts_toggle,caps:ctrl_modifier,altwin:swap_lalt_lwin"
