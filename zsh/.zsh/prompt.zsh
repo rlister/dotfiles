76ec4e3d4b423195711cd43f68f7de92e81b17dc
Richard Lister <rlister@gmail.com>
2021-01-30 21:51:27 -0500

    better setup for emacs-vterm and prompt jumping

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -13,24 +13,46 @@ zstyle ':vcs_info:*' unstagedstr   '%F{red}-'
 zstyle ':vcs_info:*' formats       ' %c%u%F{green}%b%f'
 zstyle ':vcs_info:*' actionformats ' %c%u%F{green}%b|%F{red}%a%f '
 
-## update vcs on every prompt
-if [ "$TERM" != "dumb" ] ; then
+## both dumb and smart prompts
+if [ "$TERM" == "dumb" ]; then
+  unsetopt zle
+  PS1='%2~%(!.#.$) '
+else
+  ## update vcs before every prompt
   precmd () {
     vcs_info
   }
+
+  ## report assume-role settings in prompt
+  function aws_account {
+    [ "$AWS_ACCOUNT_NAME" ] && [ "$AWS_ACCOUNT_ROLE" ] && echo "%F{blue}$AWS_ACCOUNT_NAME/$AWS_ACCOUNT_ROLE%f"
+  }
+
+  PROMPT=$'%F{blue}%T %F{cyan}%2~ %F{green}%L:${AWS_VAULT:+$AWS_VAULT}:$(aws_account)${vcs_info_msg_0_}%f%(!.#.$) '
 fi
 
-[[ $TERM == "dumb" ]] && unsetopt zle && PS1='$ '
-
-## report assume-role settings in prompt
-function aws_account {
-  [ "$AWS_ACCOUNT_NAME" ] && [ "$AWS_ACCOUNT_ROLE" ] && echo "%F{blue}$AWS_ACCOUNT_NAME/$AWS_ACCOUNT_ROLE%f"
-}
-
-PROMPT=$'%F{blue}%T %F{cyan}%2~ %F{green}%L:${AWS_VAULT:+$AWS_VAULT}:$(aws_account)${vcs_info_msg_0_}%f%(!.#.$) '
-
+## emacs vterm hackage
 if [ "$INSIDE_EMACS" == "vterm" ]; then
-  chpwd() {
-    print -Pn "\e]51;A%~/\e\\";
+  ## used below
+  vterm_printf() {
+    printf "\e]%s\e\\" "$1"
   }
+
+  ## allow vterm to clear scrollback with C-c C-l
+  alias clear='vterm_printf "51;Evterm-clear-scrollback";tput clear'
+
+  ## magic at end of prompt for vterm to use in vterm-previous-prompt
+  vterm_prompt_end() {
+    vterm_printf "51;A$(whoami)@$(hostname):$(pwd)";
+  }
+
+  setopt PROMPT_SUBST
+  PROMPT=$PROMPT'%{$(vterm_prompt_end)%}'
+
+  ## set terminal title for vterm-buffer-name-string to use
+  autoload -U add-zsh-hook
+  add-zsh-hook -Uz chpwd (){ print -Pn "\e]2;%~\a" }
+
+  ## start shell with correct title
+  print -Pn "\e]2;%~\a"
 fi
