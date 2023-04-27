ade73d5d2370d3fb5ffe19755f049c100fa8f92b
Richard Lister <rlister@gmail.com>
2023-04-27 17:23:32 -0400

    min font

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -131,4 +131,4 @@ c.fonts.web.family.fantasy = 'Source Serif Pro'
 c.fonts.web.family.fixed = 'Roboto Mono'
 c.fonts.web.size.default = 18
 c.fonts.web.size.default_fixed = 14
-c.fonts.web.size.minimum = 18
+c.fonts.web.size.minimum = 10
